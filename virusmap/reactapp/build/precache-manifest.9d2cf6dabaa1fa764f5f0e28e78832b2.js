d18c83eb351d6755340d81d497e255009aa079eb
YufeiZheng <2415571409@qq.com>
2020-04-14 10:14:03 +0800

    Add files via upload

---
+++ virusmap/reactapp/build/precache-manifest.9d2cf6dabaa1fa764f5f0e28e78832b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5b2fc04f036e8108f2d49eaca46b6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afbe6916e938029c5ed6",
+    "url": "./static/css/main.d5ecfbe6.chunk.css"
+  },
+  {
+    "revision": "ff14f2735247d443ab7f",
+    "url": "./static/js/2.64a55df1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.64a55df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbe6916e938029c5ed6",
+    "url": "./static/js/main.1afe499a.chunk.js"
+  },
+  {
+    "revision": "c7c9d123cd6a0eacd1f1",
+    "url": "./static/js/runtime-main.b8411b11.js"
+  },
+  {
+    "revision": "1d447e69cb89ae20bae3b6a91f785317",
+    "url": "./static/media/banner.1d447e69.jpg"
+  }
+]);
